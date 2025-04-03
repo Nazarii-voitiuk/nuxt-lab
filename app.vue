1c2740a140b0bf38e473e3374b603c2440561ddb
Nazarii-voitiuk <nazarii.voitiuk@oa.edu.ua>
2025-04-03 15:32:00 +0300

    feat: add product list with search, sorting and pagination

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-black text-white min-h-screen">
+    <nav class="bg-gray-800 p-4 flex justify-center">
+      <NuxtLink to="/products" class="text-white font-semibold hover:underline">
+        Перейти до списку продуктів
+      </NuxtLink>
+    </nav>
+    <NuxtPage />
   </div>
 </template>
